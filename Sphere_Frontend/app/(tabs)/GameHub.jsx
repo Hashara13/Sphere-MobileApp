ebc09029229752b6d191483227bb9fe4adfea06d
Hashara13 <hnethmin13@gmail.com>
2024-08-22 12:50:10 +0530

    (tabs) added

---
+++ Sphere_Frontend/app/(tabs)/GameHub.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GameHub = () => {
+  return (
+    <View>
+      <Text>GameHub</Text>
+    </View>
+  )
+}
+
+export default GameHub
